4f91f4255e90e39f9bb068007bd86fff582ae8ad
Gabriel S <sirlopug@gmail.com>
2021-01-21 11:16:55 -0800

    initial commit

---
+++ src/main/java/com/sirlopu/stgjms/StgJmsApplication.java
@@ -0,0 +1,13 @@
+package com.sirlopu.stgjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StgJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StgJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirlopu/stgjms/StgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirlopu.stgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
